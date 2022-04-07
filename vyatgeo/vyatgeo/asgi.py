8e06eae169cf1e2a925b491b7aae4e0a30432dcc
Markov Roman <markovrv@yandex.ru>
2022-04-07 23:46:06 +0300

    init

---
+++ vyatgeo/vyatgeo/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for vyatgeo project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'vyatgeo.settings')
+
+application = get_asgi_application()

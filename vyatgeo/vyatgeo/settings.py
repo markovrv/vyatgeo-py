e3eab978d53c158c876194e6953dafde97ab5dec
Markov Roman <markovrv@yandex.ru>
2022-04-08 00:04:24 +0300

    init

--- vyatgeo/vyatgeo/settings.py
+++ vyatgeo/vyatgeo/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'lang_backend.apps.LangBackendConfig',
+    'lang.apps.LangBackendConfig',
 ]
 
 MIDDLEWARE = [
--- vyatgeo/vyatgeo/urls.py
+++ vyatgeo/vyatgeo/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from lang_backend import views as lang_views
+from lang import views as lang_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
